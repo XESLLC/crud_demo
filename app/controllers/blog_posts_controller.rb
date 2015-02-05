de06c32f70ed12fab7fc4d8aec9bb47ac2a06f6b
David Abramowitz <david.xesllc@gmail.com>
2015-02-04 22:03:06 -0700

    Story 1

--- app/controllers/blog_posts_controller.rb
+++ app/controllers/blog_posts_controller.rb
@@ -4,8 +4,24 @@ class BlogPostsController < ApplicationController
    @blog_post = BlogPost.new
  end
 
+  def create
+   blog_post = BlogPost.new(blog_post_params)
+   if blog_post.save
+     redirect_to blog_posts_path
+   else
+     @blog_post = BlogPost.new(blog_post_params)
+     render :new
+   end
+  end
+
+  def index
+    @blog_posts = BlogPost.all
+  end
+
+
+ private
 
  def blog_post_params
-   params.require(:blog_posts).permit(:title, :author, :content)
+   params.require(:blog_post).permit(:title, :author, :content)
   end
 end
